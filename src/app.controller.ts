2f2298dfd7c011bd62a9fd2a25bc4013d14ee151
Carlos Ramirez <carlos@extrasistemas.com>
2022-05-16 00:19:05 -0500

    added page about and added images in home

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -4,5 +4,25 @@ import { Controller, Get, Render } from '@nestjs/common';
 export class AppController {
   @Get('/')
   @Render('index')
-  index() {}
+  index() {
+    const viewData = [];
+    viewData['title'] = 'Home Page - Online Store';
+    return {
+      viewData: viewData,
+    };
+  }
+
+  @Get('/about')
+  @Render('about')
+  about() {
+    const viewData = [];
+    viewData['title'] = 'About us - Online Store';
+    viewData['subtitle'] = 'About us';
+    viewData['description'] = 'This is an about page ...';
+    viewData['author'] = 'Developed by: Carlos Enrique Ramírez Flores';
+
+    return {
+      viewData: viewData,
+    };
+  }
 }
