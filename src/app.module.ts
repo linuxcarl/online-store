e858e58eec8e7228409085eccbd2fed8fa1707ca
Carlos Ramirez <carlos@extrasistemas.com>
2022-05-17 09:27:19 -0500

    added seccion of products

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-
+import { ProductsController } from './products.controller';
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, ProductsController],
   providers: [],
 })
 export class AppModule {}
