c72e547fa8e20fb29d50576e1e00f6077ba603d0
Carlos Ramirez <carlos@extrasistemas.com>
2022-05-25 08:57:42 -0500

    added form by create products

--- src/admin/admin.products.controller.ts
+++ src/admin/admin.products.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Render } from '@nestjs/common';
+import { Controller, Get, Render, Post, Redirect, Body } from '@nestjs/common';
 import { ProductsService } from '../models/product.service';
+import { Product } from '../models/product.entity';
 @Controller('admin/products')
 export class AdminProductsController {
   constructor(private readonly productService: ProductsService) {}
@@ -11,5 +12,16 @@ export class AdminProductsController {
     viewData['title'] = 'Admin Products - Admin online store';
     viewData['products'] = await this.productService.findAll();
     return { viewData };
+  }
+
+  @Post('/store')
+  @Redirect('/admin/products/')
+  async store(@Body() body): Promise<void> {
+    const newProduct = new Product();
+    newProduct.setName(body.name);
+    newProduct.setDescription(body.description);
+    newProduct.setPrice(body.price);
+    newProduct.setImage('game.png');
+    await this.productService.createOrUpdate(newProduct);
   }
 }
--- src/models/product.service.ts
+++ src/models/product.service.ts
@@ -17,4 +17,7 @@ export class ProductsService {
   findOne(id: string): Promise<Product> {
     return this.productsRepository.findOne(id);
   }
+  createOrUpdate(product: Product): Promise<Product> {
+    return this.productsRepository.save(product);
+  }
 }
