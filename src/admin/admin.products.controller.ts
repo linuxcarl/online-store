c0d128c51da6b71d48bd9ed98df903a013a4bb96
Carlos Ramirez <carlos@extrasistemas.com>
2022-05-26 00:27:45 -0500

    added library multer by upload image in products

--- src/admin/admin.products.controller.ts
+++ src/admin/admin.products.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Render, Post, Redirect, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Render,
+  Post,
+  Redirect,
+  Body,
+  UseInterceptors,
+  UploadedFile,
+} from '@nestjs/common';
 import { ProductsService } from '../models/product.service';
 import { Product } from '../models/product.entity';
+import { FileInterceptor } from '@nestjs/platform-express';
 @Controller('admin/products')
 export class AdminProductsController {
   constructor(private readonly productService: ProductsService) {}
@@ -15,13 +25,17 @@ export class AdminProductsController {
   }
 
   @Post('/store')
+  @UseInterceptors(FileInterceptor('image', { dest: './public/uploads' }))
   @Redirect('/admin/products/')
-  async store(@Body() body): Promise<void> {
+  async store(
+    @Body() body,
+    @UploadedFile() file: Express.Multer.File,
+  ): Promise<void> {
     const newProduct = new Product();
     newProduct.setName(body.name);
     newProduct.setDescription(body.description);
     newProduct.setPrice(body.price);
-    newProduct.setImage('game.png');
+    newProduct.setImage(file.filename);
     await this.productService.createOrUpdate(newProduct);
   }
 }
