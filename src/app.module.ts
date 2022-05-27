64c35a33a71460d9554c9a6589e72a50f6c5d4f1
Carlos Ramirez <carlos@extrasistemas.com>
2022-05-27 09:19:38 -0500

    added register users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,24 @@
-import { Global, Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
+import { Module, Global } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { ProductsController } from './products.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersService } from './models/users.service';
 import { Product } from './models/product.entity';
-import { ProductsService } from './models/product.service';
+import { User } from './models/user.entity';
 import { AdminModule } from './admin/admin.module';
+import { AuthModule } from './auth/auth.module';
+import { ProductsService } from './models/product.service';
 
 @Global()
 @Module({
   imports: [
     TypeOrmModule.forRoot(),
-    TypeOrmModule.forFeature([Product]),
+    TypeOrmModule.forFeature([Product, User]),
     AdminModule,
+    AuthModule,
   ],
   controllers: [AppController, ProductsController],
-  providers: [ProductsService],
-  exports: [ProductsService],
+  providers: [ProductsService, UsersService],
+  exports: [ProductsService, UsersService],
 })
 export class AppModule {}
