396fed7a6cefa6254e1b4b6a79dff90121793877
Carlos Ramirez <carlos@extrasistemas.com>
2022-05-30 23:49:43 -0500

    added login

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,16 @@
-import { Controller, Get, Render, Post, Redirect, Body } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Render,
+  Post,
+  Body,
+  Res,
+  Req,
+  Redirect,
+} from '@nestjs/common';
 import { User } from 'src/models/user.entity';
 import { UsersService } from '../models/users.service';
+import * as session from 'express-session';
 
 @Controller('/auth')
 export class AuthController {
@@ -27,5 +37,35 @@ export class AuthController {
     newUser.setRole('client');
     newUser.setBalance(1000);
     await this.usersService.createOrUpdate(newUser);
+  }
+  @Get('/login')
+  @Render('auth/login')
+  login() {
+    const viewData = [];
+    viewData['title'] = 'Login - Online store';
+    viewData['subtitle'] = 'User Login';
+    return { viewData };
+  }
+
+  @Post('/connect')
+  async connect(@Body() body, @Req() request, @Res() response) {
+    const { email, password } = body;
+    const user = await this.usersService.login(email, password);
+    if (user) {
+      request.session.user = {
+        id: user.getId(),
+        name: user.getName(),
+        role: user.getRole(),
+      };
+      return response.redirect('/');
+    } else {
+      return response.redirect('/auth/login');
+    }
+  }
+
+  @Get('/logout')
+  @Redirect('/')
+  logout(@Req() resquest) {
+    resquest.session.user = null;
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,20 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import * as hbs from 'hbs';
 import * as hbsUtils from 'hbs-utils';
-
+import * as session from 'express-session';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(
+    session({
+      secret: 'nest-book',
+      resave: false,
+      saveUninitialized: false,
+    }),
+  );
+  app.use(function (req, res, next) {
+    res.locals.session = req.session;
+    next();
+  });
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   hbs.registerPartials(join(__dirname, '..', 'views/layouts'));
