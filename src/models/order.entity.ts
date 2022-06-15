fd1dc80e76397b9114d4013317ce849287ad5d0e
Carlos Ramirez <carlos@extrasistemas.com>
2022-06-15 08:46:53 -0500

    added model by orders

---
+++ src/models/order.entity.ts
@@ -0,0 +1,58 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  ManyToMany,
+  ManyToOne,
+  OneToMany,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import { Item } from './item.entity';
+import { User } from './user.entity';
+@Entity()
+export class Order {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  total: number;
+
+  @CreateDateColumn()
+  date: Date;
+  @ManyToOne(() => User, (user) => user.orders)
+  user: User;
+
+  @OneToMany(() => Item, (item) => item.order)
+  items: Item[];
+
+  getId(): number {
+    return this.id;
+  }
+  setId(id: number): void {
+    this.id = id;
+  }
+  getTotal(): number {
+    return this.total;
+  }
+  setTotal(total: number): void {
+    this.total = total;
+  }
+  getDate(): Date {
+    return this.date;
+  }
+  setDate(date: Date): void {
+    this.date = date;
+  }
+  getUser(): User {
+    return this.user;
+  }
+  setUser(user: User): void {
+    this.user = user;
+  }
+  getItems(): Item[] {
+    return this.items;
+  }
+  setItem(items: Item[]): void {
+    this.items = items;
+  }
+}
